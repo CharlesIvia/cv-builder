ad802c7864ec103d1761c65f0e3fc73c9b407b4e
Charles Ivia <iviacharles@gmail.com>
2021-11-06 22:06:44 +0300

    work component

---
+++ src/components/Work.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Work({ place, position, start, end, roles }) {
+  return (
+    <div>
+      <div>
+        <p>
+          <span>{place || "Facebook, Menlo Park"} </span>
+          <span> - </span>
+          <span>{position || "Crab Trainer"}</span>
+          <span></span>
+        </p>
+      </div>
+      <div>
+        <p>
+          <span>{start || "August 2020"}</span>
+          <span> - </span>
+          <span>{end || "Today"}</span>
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>Extreme pen spinning</li>
+          {roles.map((role) => (
+            <li key={roles.indexOf(role)}>{role.rolename}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
